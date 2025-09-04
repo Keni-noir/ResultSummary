c87eae032033d2fb6b4c35ed6023be4e9865a6f6
Goodness Ajayi <keninoir03@gmail.com>
2025-09-05 00:45:53 +0100

    second commit

--- src/Component/items.jsx
+++ src/Component/items.jsx
@@ -3,7 +3,7 @@ function items(){
     return(
         <div>
             {Data.map((item) => (
-                <div key={item.id} className=" flex space-between p-3 gap-72 mb-4 bg-gray-100 rounded-md">
+                <div key={item.id} className=" flex space-between p-3 gap-10 md:gap-72  mb-4 bg-gray-100 rounded-md">
                     <div className='flex gap-3'>
                         <img src={item.icon} alt="" className='w-10 h-6'/>
                         <h2 className="text-font-semibold text-gray-800 ">{item.category}</h2>
